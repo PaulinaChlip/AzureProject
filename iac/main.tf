37d192dea2719f5a1228e874740692141cc1b484
Paulina Chlipała <147342518+PaulinaChlip@users.noreply.github.com>
2026-02-11 19:31:18 +0100

    Update main.tf

--- iac/main.tf
+++ iac/main.tf
@@ -11,29 +11,9 @@ provider "azurerm" {
   features {}
 }
 
-#resource "azurerm_resource_group" "rg" {
- # name     = "iad-lab-rg"
-  # ZMIANA: Zaktualizowano region na dostępny w subskrypcji studenckiej
-  #location = "Poland Central"
-#}
-
 data "azurerm_resource_group" "rg" {
   name = var.resource_group_name
 }
-
-resource "random_string" "random" {
-  length  = 8
-  special = false
-  upper   = false
-}
-
-#resource "azurerm_storage_account" "storage" {
- # name                     = "iadlabstorage${random_string.random.result}"
- # resource_group_name      = azurerm_resource_group.rg.name
- # location                 = azurerm_resource_group.rg.location
- # account_tier             = "Standard"
- # account_replication_type = "LRS" # Locally-redundant storage (najtańsza opcja)
-#}
 
 resource "azurerm_eventhub_namespace" "namespace" {
   name                = var.eventhub_namespace_name
